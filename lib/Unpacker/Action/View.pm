2a60d39332a822cc32300700546b30d8321b3d3e
vti <viacheslav.t@gmail.com>
2011-05-09 01:15:57 +0300

    Fixed content length setting

--- lib/Unpacker/Action/View.pm
+++ lib/Unpacker/Action/View.pm
@@ -8,19 +8,25 @@ use base 'Unpacker::Action';
 use Lamework::Registry;
 use Unpacker::Archive;
 use Unpacker::App::Directory;
+use Plack::Util;
 
 sub run {
     my $self = shift;
 
     my $home = Lamework::Registry->get('home');
-    my $app = Unpacker::App::Directory->new(
+    my $app  = Unpacker::App::Directory->new(
         {root => $home->catfile('htdocs/uploads')})->to_app;
 
     my $res = $app->($self->env);
 
     my $body = $res->[2];
+
     $self->res->code($res->[0]);
-    $self->res->headers([@{$res->[1]}, 'Content-Length' => length($body->[0])]);
+    $self->res->headers(
+        [   @{$res->[1]},
+            'Content-Length' => Plack::Util::content_length($body)
+        ]
+    );
     $self->res->body($body);
 }
 
